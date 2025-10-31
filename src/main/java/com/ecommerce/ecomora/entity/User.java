c6b001946550e688f062dab6f898d24ba0ef1194
hafidzph <hafidzph1@gmail.com>
2025-11-01 00:26:54 +0700

    first commit

---
+++ src/main/java/com/ecommerce/ecomora/entity/User.java
@@ -0,0 +1,5 @@
+package com.ecommerce.ecomora.entity;
+
+public class User {
+
+}
---
+++ src/test/java/com/ecommerce/ecomora/EcomoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
